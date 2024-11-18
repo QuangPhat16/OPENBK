af39f054bb2c1f4a5c0becc2dad51596c0a5b5cc
Phan Nguyễn Hữu Phước <138502336+phacko11@users.noreply.github.com>
2024-11-18 15:39:55 +0700

    Fix commentModel

--- src/database/models/commentModel.js
+++ src/database/models/commentModel.js
@@ -1,38 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../src/database');
 
-class Comment extends Model {}
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {}
 
-Comment.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  text: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  date: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  parentId: {
-    type: DataTypes.INTEGER,
-    allowNull: true
-  }
-}, {
-  sequelize,
-  modelName: 'Comment'
-});
+  Comment.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    parentId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Comment'
+  });
 
-Comment.associate = (models) => {
-  Comment.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
-  Comment.hasMany(models.Comment, { foreignKey: 'parentId', as: 'replies' });
+  return Comment;
 };
-
-module.exports = Comment;
